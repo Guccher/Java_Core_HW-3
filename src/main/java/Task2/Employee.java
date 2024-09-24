87846873e02687503ea5f7fae40a7ad4eb268190
Guccher <Guccher_007@mail.ru>
2024-09-24 21:18:51 +0300

    Add file Task2 Employee

--- src/main/java/Task2/Employee.java
+++ src/main/java/Task2/Employee.java
@@ -1,4 +1,25 @@
 package Task2;
 
 public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
+
+
