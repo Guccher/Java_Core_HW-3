1878c50144afc2437dda853d3ec572f0cf3bb707
Guccher <Guccher_007@mail.ru>
2024-09-24 21:19:23 +0300

    Add file Task2 Main

--- src/main/java/Task2/Main.java
+++ src/main/java/Task2/Main.java
@@ -1,4 +1,23 @@
 package Task2;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("John Doe", 3000));
+        employees.add(new Manager("Jane Smith", 5000));
+        employees.add(new Employee("Mark Johnson", 4000));
+
+        System.out.println("Before salary increase:");
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+
+    }
 }
